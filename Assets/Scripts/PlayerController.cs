44cacf1c83876e54e403c5f2eebc69b853d51b2d
Pratik Mulguthi <pmulguthi12@gmail.com>
2026-01-27 10:33:51 +0545

    Nezuko model and animation added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,16 +2,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 3f;      
-    public float rotationSpeed = 200f; 
+    public float moveSpeed = 5f;      
+    public float rotationSpeed = 500f; 
 
     CameraController cameraController;
     Quaternion targetRotation;
+    Animator animator;
+    CharacterController characterController;
 
     private void Awake()
     {
       cameraController = Camera.main.GetComponent<CameraController>();
-         
+       animator = GetComponent<Animator>();
+       characterController = GetComponent<CharacterController>();
     }
 
     private void Update()
@@ -20,15 +23,20 @@ public class PlayerController : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis ("Vertical");
 
+        float moveAmount = Mathf.Clamp01(Mathf.Abs(h) + Mathf.Abs(v));
+
        var moveInput = (new Vector3(h,0,v)).normalized;
         var moveDir = cameraController.PlanerRotation * moveInput;
 
-        if (moveInput.magnitude > 0)
+        if (moveAmount > 0)
         {
-            transform.position += moveDir * moveSpeed * Time.deltaTime;
+            characterController.Move(moveDir * moveSpeed * Time.deltaTime);
+            
             targetRotation = Quaternion.LookRotation(moveDir);       
         }
         
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime); 
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        animator.SetFloat("moveAmount", moveAmount, 0.2f, Time.deltaTime);
     }
 }
